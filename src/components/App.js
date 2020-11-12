13c2a4407ef4e125326014be814c05c3bb2dceb7
Almudena <almudenablanco76@gmail.com>
2020-11-12 09:27:51 +0100

    Create components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import PokeList from "./PokeList";
 import pokemons from "../data/pokemons.json";
+
 import "../stylesheets/App.scss";
 
 class App extends React.Component {
   render() {
-    return <>Hola mundo</>;
+    return <PokeList />;
   }
 }
 
